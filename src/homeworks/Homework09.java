c86cf19f3ea9bfa794eb7ea2bd8528b8950c37b2
Abdallah Jaber <abdallahj708@gmail.com>
2022-11-22 18:10:31 -0600

    started Homework09

---
+++ src/homeworks/Homework09.java
@@ -0,0 +1,11 @@
+package homeworks;
+
+public class Homework09 {
+    public static void main(String[] args) {
+        System.out.println("---Task 1---");
+
+        int[] num = {-4, 0, -7, 0, 5, 10, 45, 45};
+
+
+    }
+}
