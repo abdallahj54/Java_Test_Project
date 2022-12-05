5549b9f7db70f8ba9c3d453b024ba1bf42cd5b0f
Abdallah Jaber <abdallahj708@gmail.com>
2022-12-04 21:08:53 -0600

    updated Project08

--- src/projects/Project08.java
+++ src/projects/Project08.java
@@ -82,7 +82,7 @@ public class Project08 {
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 1; i++) {
             if(nums[i] + 1 != nums[i + 1]) return nums[i] + 1;
-
-        } return -1;
+        }
+        return -1;
     }
 }
